80d196ad775197c2619c824c8ffbc11ec6165f42
GustavoGiachelin <68574398+GustavoGiachelin@users.noreply.github.com>
2022-01-27 21:21:07 -0300

    css

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firebase-crud'`, () => {
+  it(`should have as title 'GustavoGiachelin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firebase-crud');
+    expect(app.title).toEqual('GustavoGiachelin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('firebase-crud app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('GustavoGiachelin app is running!');
   });
 });
